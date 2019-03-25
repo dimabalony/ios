bf17564c734974f4f7792a464e6ade8c53ddb1a6
Dima Bolonikov <bolonickov.dmitry@icloud.com>
2019-03-25 23:58:30 +0300

    finished 5

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/5/Sorted.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/5/Sorted.m
@@ -7,88 +7,75 @@
 
 // Complete the sorted function below.
 - (ResultObject*)sorted:(NSString*)string {
-    ResultObject *value = [ResultObject new];
+    ResultObject *value = [[ResultObject new] autorelease];
     NSMutableArray *array = [[string componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] mutableCopy];
     
-    NSInteger leftBorder = 0;
-    NSInteger rightBorder = 0;
+    NSInteger leftBorder = -1;
     NSInteger index = 0;
-    BOOL isPossibleToSort = YES;
-    BOOL isSwapable = NO;
+    BOOL possibleToReverse = NO;
     while (index < [array count] - 1) {
         if ([array[index] integerValue] > [array[index + 1] integerValue]) {
-            if (leftBorder + rightBorder > 0) {
-                isPossibleToSort = NO;
+            if (leftBorder == -1) {
+                leftBorder = index;
+            } else if (index - leftBorder == 1) {
+                possibleToReverse = YES;
                 break;
             }
-            leftBorder = index;
-            while (index < [array count] - 1) {
-                if ([array[index] integerValue] < [array[index + 1] integerValue]) {
-                    break;
-                }
-                index++;
-            }
-            rightBorder = index;
-            for (NSInteger indexToReverse = 0; indexToReverse + leftBorder <= (leftBorder + rightBorder) / 2; indexToReverse++) {
-                id temp = array[leftBorder + indexToReverse];
-                array[leftBorder + indexToReverse] = array[rightBorder - indexToReverse];
-                array[rightBorder - indexToReverse] = temp;
-            }
-            index = -1;
+        } else if ((leftBorder != -1) && (index - leftBorder != 1)) {
+            break;
         }
         index++;
     }
     
-    //if isPossibleToSort == NO && (rightBorder - leftBorder == 1)
-    //найти второе несоответствие
-    //поменять их
-    //1 2 3 7 5 6 4 8 9
-    //1 2 3 5 7 6 4 8 9
-    
-    if (!isPossibleToSort && (rightBorder - leftBorder == 1)) {
-        
-        id temp = array[leftBorder];
-        array[leftBorder] = array[rightBorder];
-        array[rightBorder] = temp;
-        
-        NSInteger index = rightBorder;
-        
-        while (index < [array count] - 1) {
-            if ([array[index] integerValue] > [array[index + 1] integerValue]) {
+    if (leftBorder != -1) {
+        NSInteger rightBorder = 0;
+        NSInteger index = [array count] - 2;
+        while (index >= 0) {
+            if ([array[index] integerValue] > [array[index + 1]  integerValue]) {
                 rightBorder = index + 1;
-                temp = array[leftBorder];
+                break;
+            }
+            index--;
+        }
+        if (rightBorder != 0) {
+            if (possibleToReverse) {
+                for (index = leftBorder; index <= (leftBorder + rightBorder) / 2; index++) {
+                    id temp = array[index];
+                    array[index] = array[rightBorder + leftBorder - index];
+                    array[rightBorder + leftBorder - index] = temp;
+                }
+            } else {
+                id temp = array[leftBorder];
                 array[leftBorder] = array[rightBorder];
                 array[rightBorder] = temp;
             }
-            index++;
-        }
-        
-        index = 0;
-        isPossibleToSort = YES;
-        while (index < [array count] - 1) {
-            if ([array[index] integerValue] > [array[index + 1] integerValue]) {
-                isPossibleToSort = NO;
+            index = 0;
+            BOOL isSorted = YES;
+            while (index < [array count] - 1) {
+                if ([array[index] integerValue] > [array[index + 1] integerValue]) {
+                    isSorted = NO;
+                }
+                index++;
             }
-            index++;
-        }
-        isSwapable = YES;
-    }
-    
-    if (isPossibleToSort) {
-        value.status = YES;
-        NSString *detail = [NSString new];
-        if (isSwapable || (rightBorder - leftBorder == 1)) {
-            detail = @"swap";
-        } else if (rightBorder - leftBorder > 1) {
-            detail = @"reverse";
-        }
-        if (rightBorder + leftBorder > 0) {
-            value.detail =  [NSString stringWithFormat:@"%@ %ld %ld", detail, leftBorder + 1, rightBorder + 1];
+            if (isSorted) {
+                value.status = YES;
+                NSString *swapOrReverse = @"";
+                if (possibleToReverse) {
+                    swapOrReverse = @"reverse";
+                } else {
+                    swapOrReverse = @"swap";
+                }
+                value.detail = [NSString stringWithFormat:@"%@ %ld %ld", swapOrReverse, leftBorder + 1, rightBorder + 1];
+            } else {
+                value.status = NO;
+            }
+        } else {
+            value.status = NO;
         }
     } else {
-        value.status = NO;
+        value.status = YES;
     }
-
+    [array release];
     return value;
 }
 
