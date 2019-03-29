0086765047e82af4df41b0b99db8d98c3b07e0f2
Dima Bolonikov <bolonickov.dmitry@icloud.com>
2019-03-30 00:54:13 +0300

    finished first task

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,24 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+-(BOOL)checkMagazine:(NSString *)magazine note:(NSString *)note {
+    NSMutableArray *magazineNotes = [[[magazine lowercaseString] componentsSeparatedByString:@" "] mutableCopy];
+    NSArray *notes = [[note lowercaseString] componentsSeparatedByString:@" "];
+    
+    for (NSString *noteWord in notes) {
+        BOOL found = NO;
+        for (NSString *magazineWord in magazineNotes) {
+            if ([noteWord isEqualToString:magazineWord]) {
+                found = YES;
+                [magazineNotes removeObject:magazineWord];
+                break;
+            }
+        }
+        if (!found) {
+            return NO;
+        }
+    }
+    
+    return YES;
+}
 @end
